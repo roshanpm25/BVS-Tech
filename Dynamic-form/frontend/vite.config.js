ab803bece6540faf2facff6ed7a94bbe72684b07
roshanpm25 <arsrosh@gmail.com>
2025-04-13 15:27:43 +0530

    update

--- Dynamic-form/frontend/vite.config.js
+++ Dynamic-form/frontend/vite.config.js
@@ -3,9 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:5000', // Proxy API requests to your backend
-    },
+  build: {
+    outDir: 'build' // Vercel uses 'build' as static folder
   },
 });
